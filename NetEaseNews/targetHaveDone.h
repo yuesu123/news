23e57d858a652da3e3d896a11bb0d3a66f383608
hjl_wg <wanggang@wanggangdeMacBook-Pro.local>
2016-06-07 23:26:00 +0800

    修改APP名称

--- NetEaseNews/targetHaveDone.h
+++ NetEaseNews/targetHaveDone.h
@@ -1,13 +1,26 @@
-//
-//  targetHaveDone.h
-//  NetEaseNews
-//
-//  Created by  汪刚 on 16/6/7.
-//  Copyright © 2016年 WackoSix. All rights reserved.
-//
 
 #ifndef targetHaveDone_h
 #define targetHaveDone_h
-
-
 #endif /* targetHaveDone_h */
+//--------已完成
+
+/*
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
